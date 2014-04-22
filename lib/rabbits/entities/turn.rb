b241506d96c8ff8cc16963cb6479f9932d752b65
Antonio Felguerez <agfelgue@gmail.com>
2014-04-22 10:47:18 -0500

    Adds Rabbit entity

---
+++ lib/rabbits/entities/turn.rb
@@ -0,0 +1,15 @@
+attr_reader :turn_id, :player_id, :game_id
+attr_accessor :shot_count, :meat_count, :roll_count
+
+module Rabbits
+	class Turn < Entity
+		def initialize(attrs)
+			@player_id = attrs[:player_id]
+			@game_id = attrs[:game_id]
+			shot_count = 0
+			meat_count = 0
+			roll_count = 0
+			Roll.new
+		end
+	end
+end
