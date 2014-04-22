9658153706a167135fff8ea4168a6d146ef879ec
Moiz Jangda <macdaddy@Moizs-MacBook-Pro.local>
2014-04-22 11:45:09 -0500

    adds player tests

--- spec/entities/player_spec.rb
+++ spec/entities/player_spec.rb
@@ -1,7 +1,11 @@
-require 'spec_helper'
+  require 'spec_helper'
 describe 'Player' do
   it 'exists' do
     expect(Rabbits::Player).to be_a(Class)
-
+  end
+  it 'is initialized with a player and a meat count' do
+    player = Rabbits::Player.new(:name => "bob")
+    expect(player.name).to eq("bob")
+    expect(player.meat_count).to eq(0)
   end
 end
