4b77f7fe6fc8486397762e732ef1c57f0ab84d36
Brandon Beasley <brandontbeasley@gmail.com>
2014-04-22 11:02:01 -0500

    added cup spec and entity

--- lib/rabbits/entities/cup.rb
+++ lib/rabbits/entities/cup.rb
@@ -1,5 +1,8 @@
 module Rabbits
   class Cup
-
+    attr_accessor :die
+      def initialize
+        @die = ["green", "green", "green", "green", "green", "green", "yellow",  "yellow",  "yellow",  "yellow", "red", "red", "red",].shuffle
+      end
   end
 end
