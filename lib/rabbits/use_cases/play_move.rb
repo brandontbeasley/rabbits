8760ddd30dde950abb1b35479cad30f3263e6a70
Antonio Felguerez <agfelgue@gmail.com>
2014-04-24 20:03:08 -0500

    walking down a black hole. everything before this works.

--- lib/rabbits/use_cases/play_move.rb
+++ lib/rabbits/use_cases/play_move.rb
@@ -1,42 +1,53 @@
 module Rabbits
 	class PlayMove < UseCase
-            def self.roll_face(array)
-             die_outcome = array.map { |die|
-                side = rand(6) + 1
-                if die == 'green'
-                  if side < 4
-                    'meat'
-                  elsif side == 6
-                    'shot'
-                  else
-                    'paw'
-                  end
-                elsif die == 'yellow'
-                  if side < 3
-                    'meat'
-                  elsif side  == 3 || side == 4
-                      'paw'
-                  else
-                    'shot'
-                  end
-                else
-                  if side < 4
-                    'shot'
-                  elsif side == 6
-                    'meat'
-                  else
-                    'paw'
-                  end
-                end
-              }
-            end
+    def roll_face(array)
+    
+    end
 
 		def run(inputs)
 			game = inputs[:game]
-                  turn = game.turns.last
-                  dice_cup = turn.cup
-                  roll = 3.times.map{ dice_cup.die.pop }
-                  rolled =
+      turn = game.turns.last
+      dice_cup = turn.cup
+      roll = 3.times.map{ dice_cup.die.pop }
+
+      die_outcome = array.map { |die|
+        side = rand(6) + 1
+        if die == 'green'
+          if side < 4
+            'meat'
+          elsif side == 6
+            'shot'
+          else
+            'paw'
+            dice_cup.push('green')
+          end
+        elsif die == 'yellow'
+          if side < 3
+            'meat'
+          elsif side  == 3 || side == 4
+            'paw'
+          else
+            'shot'
+          end
+        else
+          if side < 4
+            'shot'
+          elsif side == 6
+            'meat'
+          else
+            'paw'
+          end
+        end
+      }
+
+      outcome = roll_face(roll)
+      outcome.each { |result| 
+        if result == 'meat'
+
+
+
+      }
+
 
   		end
 	end
--- spec/use_cases/play_move_spec.rb
+++ spec/use_cases/play_move_spec.rb
@@ -1,9 +1,7 @@
 describe Rabbits::PlayMove do
 
 	let (:game) { Rabbits::StartGame.run(:players => ["john", "paul", "george"]).game}
-	before do
-		Rabbits::PlayMove.any_instance.stub(:roll).and_return(%w{meat meat meat})
-	end
+	
 	it 'exists' do
 		expect(Rabbits::PlayMove).to be_a Class
 	end
@@ -19,11 +17,17 @@ describe Rabbits::PlayMove do
 
 	it "has roll_face method that returns the outcome" do
 		die = ['red', 'yellow', 'green']
-		result = described_class.roll_face(die)
+		result = subject.roll_face(die)
 		expect(result.count).to eq 3
 		expect(result[0]).to match (/^meat|paw|shot$/)
 	end
 
-	it
+	it 'pushes the back in if result is paw' do
+		expect(subject).to receive(:roll_face).and_return(['paw', 'paw', 'paw'])
+		subject.run(game: game)
+		dice_cup = game.turns.last.cup
+		expect(dice_cup.die.count).to eq 13
+	end
+
 
 end
